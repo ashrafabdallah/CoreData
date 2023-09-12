3cd88f7314af680cbb79cc17266a0342b3e75042
Ashraf <ashrafeltantawy252@gmail.com>
2023-09-13 02:37:26 +0300

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Ashraf Eltantawy on 13/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
